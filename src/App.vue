480e1a64ceeb29f8a30d36751d27cf94b50927f6
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-11-21 11:39:56 +0100

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <HeaderComponent/>
+
+  </div>
+</template>
+
+<script>
+  import { store } from './data/store.js';
+  import axios from 'axios';
+  import HeaderComponent from './components/HeaderComponent.vue'
+
+  export default {
+    components: {
+      HeaderComponent
+    },
+
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
